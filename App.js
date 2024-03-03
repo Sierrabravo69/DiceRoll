3c6f7e06da77e73d2c613b727d7e8a0541c3bb6b
Sierrabravo69 <bhargavsai1203@gmail.com>
2024-03-03 21:10:13 +0530

    Dice and haptic feed back

--- App.js
+++ App.js
@@ -1,117 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { StyleSheet, Text, View,Image } from 'react-native'
+import React, { useState } from 'react'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import diceOne from './assets/One.png'
+import diceTwo from './assets/Two.png'
+import diceThree from './assets/Three.png'
+import diceFour from './assets/Four.png'
+import diceFive from './assets/Five.png'
+import diceSix from './assets/Six.png'
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable'
+import {trigger} from 'react-native-haptic-feedback'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Optional configuration
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+const Dice = ({imageUrl})=>{
+  console.log("imageUrl", imageUrl);
+  return(
+    <View>
+      <Image  style={styles.diceImage} source={imageUrl}/>
     </View>
-  );
-};
+  )
+}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [diceImage, setDiceImage] = useState(diceOne)
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const rollDice = () => {
+    let number = Math.floor(Math.random()*6)+1
+    console.log(number);
+    // console.log(diceImage, "asldfajsldkf");
 
+    switch (number) {
+      case 1: {
+        setDiceImage(diceOne)
+        break;
+      }
+        
+      case 2:
+        setDiceImage(diceTwo)
+        break;
+      case 3:
+        setDiceImage(diceThree)
+        break;
+      case 4:
+        setDiceImage(diceFour)
+        break;
+      case 5:
+        setDiceImage(diceFive)
+        break;
+      case 6:
+        setDiceImage(diceSix)
+        break;
+    
+      default:
+        setDiceImage(diceOne)
+        break;
+    }
+    // Trigger haptic feedback
+    trigger("impactLight", options);
+  }
+console.log("diceImage", diceImage);
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage}/>
+      {/* <Image source={diceOne}/> */}
+      <Pressable onPress={rollDice}>
+        <Text style={styles.rollDiceBtnText}>Roll the Dice</Text>
+      </Pressable>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  diceContainer: {
+    margin: 12,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  diceImage: {
+    width: 200,
+    height: 200,
   },
-  highlight: {
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
     fontWeight: '700',
+    textTransform: 'uppercase',
   },
-});
-
-export default App;
+})
